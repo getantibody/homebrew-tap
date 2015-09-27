8695d6cd2c1057a59db0813b1a9edcd89906d3c5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-09-27 03:50:47 -0300

    Update antibody.rb

--- antibody.rb
+++ antibody.rb
@@ -1,9 +1,9 @@
 class Antibody < Formula
-  desc "Faster and simpler version of antigen written in Golang"
-  homepage "http://github.com/caarlos0/antibody"
-  url "https://github.com/caarlos0/antibody/releases/download/v0.3.5/antibody_darwin_amd64.tar.gz"
-  sha256 "34c5cf646fb07380232e24252da3b8469018f86378453108c7ef8e771c9a0087"
-  version "0.3.5"
+  desc "Faster and simpler antigen written in Golang"
+  homepage "http://antibody.elasticbeanstalk.com"
+  url "http://antibody.elasticbeanstalk.com/latest/#{`uname -s`}/#{`uname -m`}"
+  version :latest
+  sha256 :no_check
   head "http://github.com/caarlos0/antibody.git"
 
   def install
@@ -12,7 +12,7 @@ class Antibody < Formula
   end
 
   test do
-    (testpath/".zshrc").write "source `brew --prefix`/share/antibody.zsh"
-    system "/bin/zsh", "--login", "-i", "-c", "antibody help"
+    (testpath/".zshrc").write "source $(brew --prefix)/share/antibody.zsh"
+    system "/bin/zsh", "--login", "-i", "-c", "antibody"
   end
 end
