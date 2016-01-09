783fe72289da503349c1317ff88f48927ae82333
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-01-09 15:51:42 -0200

    Update antibody.rb

--- antibody.rb
+++ antibody.rb
@@ -1,7 +1,7 @@
 class Antibody < Formula
   desc "Faster and simpler antigen written in Golang"
   homepage "http://antibody.elasticbeanstalk.com"
-  url "https://github.com/getantibody/antibody/releases/download/v0.3.5/antibody_#{`uname -s`.gsub(/\n/, '').downcase}_#{`uname -m`.gsub(/\n/, '').gsub("x86_64", "amd64")}.tar.gz"
+  url "https://github.com/getantibody/antibody/releases/download/v0.4.0/antibody_#{`uname -s`.gsub(/\n/, '').downcase}_#{`uname -m`.gsub(/\n/, '').gsub("x86_64", "amd64")}.tar.gz"
   version "0.4.0"
   head "http://github.com/getantibody/antibody.git"
 
