cff0d1e1c5e1c0dbed3b25077cfc186614f0858d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-04-06 21:10:59 -0300

    updated

--- antibody.rb
+++ antibody.rb
@@ -1,21 +1,20 @@
 class Antibody < Formula
   desc "Faster and simpler antigen written in Golang"
   homepage "http://antibody.elasticbeanstalk.com"
-  url "https://github.com/getantibody/antibody/releases/download/v0.4.4/antibody_#{`uname -s`.gsub(/\n/, '')}_#{`uname -m`.gsub(/\n/, '')}.tar.gz"
-  version "0.4.4"
+  url "https://github.com/getantibody/antibody/releases/download/v0.5.0/antibody_#{`uname -s`.gsub(/\n/, '')}_#{`uname -m`.gsub(/\n/, '')}.tar.gz"
+  version "0.5.0"
   head "http://github.com/getantibody/antibody.git"
 
   def install
     share.install "antibody"
-    share.install "antibody.zsh"
   end
   
   def caveats
-    "To start using antibody, you need to add `source $(brew --prefix)/share/antibody.zsh` to your `~/.zshrc`."
+    "To start using antibody, you need to add `source <(antibody init)` to your `~/.zshrc`."
   end
 
   test do
-    (testpath/".zshrc").write "source $(brew --prefix)/share/antibody.zsh"
+    (testpath/".zshrc").write "source <(antibody init)"
     system "/bin/zsh", "--login", "-i", "-c", "antibody"
   end
 end
