42df13842db836a028c938efb16ebe4444c584a4
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-08-17 20:07:47 -0300

    1.0.0

--- antibody.rb
+++ antibody.rb
@@ -1,8 +1,8 @@
 class Antibody < Formula
   desc "Faster and simpler antigen written in Golang"
   homepage "http://getantibody.github.io/"
-  url "https://github.com/getantibody/antibody/releases/download/v0.9.2/antibody_#{`uname -s`.gsub(/\n/, '')}_#{`uname -m`.gsub(/\n/, '')}.tar.gz"
-  version "0.9.2"
+  url "https://github.com/getantibody/antibody/releases/download/v1.0.0/antibody_#{`uname -s`.gsub(/\n/, '')}_#{`uname -m`.gsub(/\n/, '')}.tar.gz"
+  version "1.0.0"
   head "http://github.com/getantibody/antibody.git"
 
   def install
