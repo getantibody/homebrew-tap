5965c52455c6dd7a71f500d8395a514619329656
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-12-29 12:49:31 -0200

    Update antibody.rb

--- antibody.rb
+++ antibody.rb
@@ -3,7 +3,8 @@ class Antibody < Formula
   homepage "http://getantibody.github.io"
   url "https://github.com/getantibody/antibody/releases/download/v2.2.1/antibody_#{%x(uname -s).gsub(/\n/, '')}_#{%x(uname -m).gsub(/\n/, '')}.tar.gz"
   head "https://github.com/getantibody/antibody.git"
-
+  version "v2.2.1"
+  
   def install
     bin.install "antibody"
   end
