8f0e8a091b4c9ca0e45b1ba3c9d0204174e3ea8e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-12-29 12:45:27 -0200

    Update antibody.rb

--- antibody.rb
+++ antibody.rb
@@ -1,7 +1,7 @@
 class Antibody < Formula
   desc "A faster and simpler antigen written in Golang."
   homepage "http://getantibody.github.io"
-  url "https://github.com/getantibody/antibody/releases/download/v2.2.1/antibody_Darwin_x86_64.tar.gz"
+  url "https://github.com/getantibody/antibody/releases/download/v2.2.1/antibody_#{%x(uname -s).gsub(/\n/, '')}_#{%x(uname -m).gsub(/\n/, '')}.tar.gz"
   head "https://github.com/getantibody/antibody.git"
 
   def install
