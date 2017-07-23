27a7d19a572ce4f77941613c0774d7aadde1ed6b
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-23 12:28:39 -0300

    antibody version v3.1.4

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -1,10 +1,10 @@
 class Antibody < Formula
-  desc "The fastest shell plugin manager."
+  desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  url "https://github.com/getantibody/antibody/releases/download/v3.1.3/antibody_Darwin_x86_64.tar.gz"
-  version "3.1.3"
-  sha256 "aa5d65aaefea5777e08df3df28521eabb990a4b9fe9ca713c7dd8c9bd8ff45c9"
-  
+  url "https://github.com/getantibody/antibody/releases/download/v3.1.4/antibody_Darwin_x86_64.tar.gz"
+  version "3.1.4"
+  sha256 "6b5a69e4890f65498cd6f2fb0e0a7c0a2abc62b74f5c25e1d17b3ac2a228f266"
+
   depends_on "git"
 
   def install
@@ -13,5 +13,9 @@ class Antibody < Formula
 
   def caveats
     "To start using antibody, you need to add `source <(antibody init)` to your `~/.zshrc`."
+  end
+
+  test do
+    
   end
 end
