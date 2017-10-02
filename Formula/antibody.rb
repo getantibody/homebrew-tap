7bd5a52ae1c7ef75652fee54fd8f034a3aa48cbb
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-02 10:15:35 -0300

    antibody version v3.3.2

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -1,9 +1,9 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  url "https://github.com/getantibody/antibody/releases/download/v3.3.1/antibody_Darwin_x86_64.tar.gz"
-  version "3.3.1"
-  sha256 "5f1914c67a935e17caec9f0ad3f4179035eeb8bc2f086aa2fc34a6ecaa03089c"
+  url "https://github.com/getantibody/antibody/releases/download/v3.3.2/antibody_Darwin_x86_64.tar.gz"
+  version "3.3.2"
+  sha256 "8da17e0b90d00891205b4cef9e4485652332cad9e4b32f9474a0158135f62143"
 
   depends_on "git"
 
