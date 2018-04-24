2c44b8d6be57e0edc8c9c8f37efaaafc64760eb4
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-23 21:24:42 -0700

    Brew formula update for antibody version v3.4.4

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -1,9 +1,9 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  url "https://github.com/getantibody/antibody/releases/download/v3.4.3/antibody_Darwin_x86_64.tar.gz"
-  version "3.4.3"
-  sha256 "a967b2343ebd588179cded3ccffde9c30a27bd8b412f54eb3d7d856066f04184"
+  url "https://github.com/getantibody/antibody/releases/download/v3.4.4/antibody_Darwin_x86_64.tar.gz"
+  version "3.4.4"
+  sha256 "c6c52571aa4df10625b35ea86d64eb44d06826161aff1de4d4e3739a636737b0"
   
   depends_on "git"
 
@@ -11,11 +11,8 @@ class Antibody < Formula
     bin.install "antibody"
   end
 
-  def caveats
-    "To start using antibody, you need to add `source <(antibody init)` to your `~/.zshrc`."
-  end
-
-  test do
-    
+  def caveats; <<~EOS
+    To start using antibody, you need to add `source <(antibody init)` to your `~/.zshrc`.
+  EOS
   end
 end
