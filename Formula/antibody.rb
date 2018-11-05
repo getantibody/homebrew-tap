a6c7e177b510880b13c984439290b15d34381581
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-05 08:45:53 -0200

    Brew formula update for antibody version v4.0.1

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -1,9 +1,9 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  url "https://github.com/getantibody/antibody/releases/download/v4.0.0/antibody_Darwin_x86_64.tar.gz"
-  version "4.0.0"
-  sha256 "14fc3e57a985f5b6509b3f1be97e837f6452310066ba8cd1fe95a937fe7b6ab5"
+  url "https://github.com/getantibody/antibody/releases/download/v4.0.1/antibody_Darwin_x86_64.tar.gz"
+  version "4.0.1"
+  sha256 "1fdbac85b82fe93a1097a6e9d402a14d600da6c1f77b7119ce2bb214c1c6dae4"
   
   depends_on "git"
 
