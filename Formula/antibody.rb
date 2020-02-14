ebb838ea0f6785ea129d6c893f8f66a571e42fe6
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-13 23:31:03 -0300

    Brew formula update for antibody version v4.3.0

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -2,24 +2,24 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  version "4.2.0"
+  version "4.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/getantibody/antibody/releases/download/v4.2.0/antibody_Darwin_x86_64.tar.gz"
-    sha256 "d1688598cd4ea325e2cbe559cfc8b35e139b329fc2458fd6e51635bfc7bc199b"
+    url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Darwin_x86_64.tar.gz"
+    sha256 "bc6361b3571f990a3f14819debb3183ad6c4be6d09b9909f996a0ea591225021"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/getantibody/antibody/releases/download/v4.2.0/antibody_Linux_x86_64.tar.gz"
-      sha256 "135827f8973d9716f7861221ec47b6b4f9a9c14b9ce7df62d2d71487fee15d5b"
+      url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_x86_64.tar.gz"
+      sha256 "4b9260f5179f50148c84cd00e3e39e0823d1288290a2fb33fdc19e603a861f50"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/getantibody/antibody/releases/download/v4.2.0/antibody_Linux_arm64.tar.gz"
-        sha256 "ea618d1712995e0b3e8b6e561688b1715f5be81e8d3b4c0384d246f09f189e54"
+        url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_arm64.tar.gz"
+        sha256 "5e980edc001b091a5c122c728bbfee7f4908321e9cc22f35c276c51dabc63a78"
       else
-        url "https://github.com/getantibody/antibody/releases/download/v4.2.0/antibody_Linux_armv6.tar.gz"
-        sha256 "55dba88d26763291f70f7fb44b34dd0c7a278243eaa57b45de20490a699f095e"
+        url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_armv6.tar.gz"
+        sha256 "ccffd9a3c088f352c8e6d81907b1ddf104f7a9ad078e395ed8042fd09937e2e5"
       end
     end
   end
