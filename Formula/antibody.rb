50bcd1eb86f3505ea3806bebac7edffe5768880e
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-14 17:35:23 -0300

    Brew formula update for antibody version v4.3.1

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -2,24 +2,24 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "http://getantibody.github.io"
-  version "4.3.0"
+  version "4.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Darwin_x86_64.tar.gz"
-    sha256 "bc6361b3571f990a3f14819debb3183ad6c4be6d09b9909f996a0ea591225021"
+    url "https://github.com/getantibody/antibody/releases/download/v4.3.1/antibody_Darwin_x86_64.tar.gz"
+    sha256 "100b2e73ae9acf20cd943ea7a6bf7538e860ad7694f52534b660f98a65bcbb6f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_x86_64.tar.gz"
-      sha256 "4b9260f5179f50148c84cd00e3e39e0823d1288290a2fb33fdc19e603a861f50"
+      url "https://github.com/getantibody/antibody/releases/download/v4.3.1/antibody_Linux_x86_64.tar.gz"
+      sha256 "ab58ef6085794ad1ce99cd59a6c59959aa5f7320c7c34d96af730afb4c05800f"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_arm64.tar.gz"
-        sha256 "5e980edc001b091a5c122c728bbfee7f4908321e9cc22f35c276c51dabc63a78"
+        url "https://github.com/getantibody/antibody/releases/download/v4.3.1/antibody_Linux_arm64.tar.gz"
+        sha256 "73d424cee0df8e0015bbaef70d9bdb99837bd028b076e0bba801cb34ddb5a0b2"
       else
-        url "https://github.com/getantibody/antibody/releases/download/v4.3.0/antibody_Linux_armv6.tar.gz"
-        sha256 "ccffd9a3c088f352c8e6d81907b1ddf104f7a9ad078e395ed8042fd09937e2e5"
+        url "https://github.com/getantibody/antibody/releases/download/v4.3.1/antibody_Linux_armv6.tar.gz"
+        sha256 "5f23fdf4e2cf2b7860c1d4366018a3e416cc47899e29b704fa8e217873571a88"
       end
     end
   end
